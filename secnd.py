2e84f0b6d0a65355263057e83ca037e5accb8821
Quratulain-Kazmi <kazmiquratulain0@gmail.com>
2026-04-27 08:28:49 -0700

    Add files via upload

---
+++ secnd.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+pass_len = 12
+charValues = string.ascii_letters + string.digits + string.punctuation
+
+#list comprehension [function for i in range(n)]
+password = "".join([random.choice(charValues) for i in range (pass_len)])
+
+print("your random is :" , password)
